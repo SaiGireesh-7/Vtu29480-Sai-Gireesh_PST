3c049c0f2ecaa2c8d8ce72ad24b703eea0f59676
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:30:53 +0530

    Format Week 6 FirstOccurrenceInString

--- week6/FirstOccurrenceInString.java
+++ week6/FirstOccurrenceInString.java
@@ -2,4 +2,9 @@
 // Question: Find the Index of the First Occurrence in a String
 // Platform: LeetCode
 // https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
-class FirstOccurrenceInString { public int strStr(String haystack,String needle){return haystack.indexOf(needle);} }
+
+class FirstOccurrenceInString {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}

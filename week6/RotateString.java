379d290e2e45a66f883087cad94e8a34d72479b4
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:30:35 +0530

    Format Week 6 RotateString

--- week6/RotateString.java
+++ week6/RotateString.java
@@ -2,4 +2,10 @@
 // Question: Rotate String
 // Platform: LeetCode
 // https://leetcode.com/problems/rotate-string/
-class RotateString { public boolean rotateString(String s,String goal){return s.length()==goal.length()&&(s+s).contains(goal);} }
+
+class RotateString {
+    public boolean rotateString(String s, String goal) {
+        return s.length() == goal.length()
+                && (s + s).contains(goal);
+    }
+}

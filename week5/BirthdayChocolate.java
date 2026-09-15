6adcf2b042cfcd362270765bf226e641f313851b
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:29:42 +0530

    Format Week 5 BirthdayChocolate

--- week5/BirthdayChocolate.java
+++ week5/BirthdayChocolate.java
@@ -1,6 +1,36 @@
 // Week 5 Task 2
-// Question: Birthday Chocolate
+// Question: The Birthday Bar / Birthday Chocolate
 // Platform: HackerRank
-// https://www.hackerrank.com/challenges/the-birthday-bar/problem
+// https://www.hackerrank.com/challenges/the-birthday-bar/
+
 import java.util.*;
-public class BirthdayChocolate { public static void main(String[]args){Scanner s=new Scanner(System.in);int n=s.nextInt();int[]a=new int[n];for(int i=0;i<n;i++)a[i]=s.nextInt();int d=s.nextInt(),m=s.nextInt(),ans=0;for(int i=0;i+m<=n;i++){int sum=0;for(int j=i;j<i+m;j++)sum+=a[j];if(sum==d)ans++;}System.out.println(ans);}}
+
+public class BirthdayChocolate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int d = sc.nextInt();
+        int m = sc.nextInt();
+        int count = 0;
+
+        for (int i = 0; i <= n - m; i++) {
+            int sum = 0;
+
+            for (int j = i; j < i + m; j++) {
+                sum += a[j];
+            }
+
+            if (sum == d) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}

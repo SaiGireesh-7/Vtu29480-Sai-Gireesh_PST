7990d98b38865bfaa59a7bc2f739bb28a6e09032
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:29:11 +0530

    Format Week 4 MoveZeroes

--- week4/MoveZeroes.java
+++ week4/MoveZeroes.java
@@ -2,4 +2,21 @@
 // Question: Move Zeroes
 // Platform: LeetCode
 // https://leetcode.com/problems/move-zeroes/
-class MoveZeroes { public void moveZeroes(int[] nums){int k=0;for(int x:nums)if(x!=0)nums[k++]=x;while(k<nums.length)nums[k++]=0;} }
+
+class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        while (k < nums.length) {
+            nums[k] = 0;
+            k++;
+        }
+    }
+}

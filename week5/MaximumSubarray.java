c33d9d2eb74d5d74e6dc67ef5381840847f13fa6
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:29:37 +0530

    Format Week 5 MaximumSubarray

--- week5/MaximumSubarray.java
+++ week5/MaximumSubarray.java
@@ -2,4 +2,17 @@
 // Question: Maximum Subarray
 // Platform: LeetCode
 // https://leetcode.com/problems/maximum-subarray/
-class MaximumSubarray { public int maxSubArray(int[] a){int cur=a[0],best=a[0];for(int i=1;i<a.length;i++){cur=Math.max(a[i],cur+a[i]);best=Math.max(best,cur);}return best;} }
+
+class MaximumSubarray {
+    public int maxSubArray(int[] a) {
+        int cur = a[0];
+        int best = a[0];
+
+        for (int i = 1; i < a.length; i++) {
+            cur = Math.max(a[i], cur + a[i]);
+            best = Math.max(best, cur);
+        }
+
+        return best;
+    }
+}

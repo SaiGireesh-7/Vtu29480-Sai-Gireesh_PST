ae4546f206601a6ed4b4aec5ffd02138f858054e
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:24:22 +0530

    Format Week 7 Parking System code

--- week7/DesignParkingSystem.java
+++ week7/DesignParkingSystem.java
@@ -1,17 +1,21 @@
 // Week 7 - Task 1
-// Question: Design Parking System. Store big, medium and small slots and make addCar return true only when a matching slot is available.
+// Question: Design Parking System
 // Platform: LeetCode
 // https://leetcode.com/problems/design-parking-system/
+
 class ParkingSystem {
     int[] slot;
+
     ParkingSystem(int big, int medium, int small) {
         slot = new int[]{0, big, medium, small};
     }
+
     public boolean addCar(int carType) {
         if (slot[carType] > 0) {
             slot[carType]--;
             return true;
         }
+
         return false;
     }
 }

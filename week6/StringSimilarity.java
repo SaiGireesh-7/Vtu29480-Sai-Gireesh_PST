9d23e691e0c1b6b2b9859bf4701844426548c7c3
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:30:24 +0530

    Format Week 6 StringSimilarity

--- week6/StringSimilarity.java
+++ week6/StringSimilarity.java
@@ -2,5 +2,30 @@
 // Question: String Similarity
 // Platform: HackerRank
 // https://www.hackerrank.com/challenges/string-similarity/
+
 import java.util.*;
-public class StringSimilarity { public static void main(String[]a){Scanner s=new Scanner(System.in);int t=s.nextInt();while(t-->0){String x=s.next();long ans=0;for(int i=0;i<x.length();i++){int j=0;while(i+j<x.length()&&x.charAt(j)==x.charAt(i+j))j++;ans+=j;}System.out.println(ans);}}}
+
+public class StringSimilarity {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String s = sc.next();
+            long answer = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                int j = 0;
+
+                while (i + j < s.length()
+                        && s.charAt(j) == s.charAt(i + j)) {
+                    j++;
+                }
+
+                answer += j;
+            }
+
+            System.out.println(answer);
+        }
+    }
+}

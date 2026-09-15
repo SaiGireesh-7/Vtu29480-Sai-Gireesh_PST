726f6aa1eb0c6539e2fe2063c7e627de0177c912
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:29:15 +0530

    Format Week 4 TransposeMatrix

--- week4/TransposeMatrix.java
+++ week4/TransposeMatrix.java
@@ -2,4 +2,19 @@
 // Question: Transpose Matrix
 // Platform: LeetCode
 // https://leetcode.com/problems/transpose-matrix/
-class TransposeMatrix { public int[][] transpose(int[][] a){int m=a.length,n=a[0].length;int[][]b=new int[n][m];for(int i=0;i<m;i++)for(int j=0;j<n;j++)b[j][i]=a[i][j];return b;} }
+
+class TransposeMatrix {
+    public int[][] transpose(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] ans = new int[n][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[j][i] = matrix[i][j];
+            }
+        }
+
+        return ans;
+    }
+}

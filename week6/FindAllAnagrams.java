c20140715368f72ee8b76683204e64a4d014413d
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:30:44 +0530

    Format Week 6 FindAllAnagrams

--- week6/FindAllAnagrams.java
+++ week6/FindAllAnagrams.java
@@ -2,5 +2,36 @@
 // Question: Find All Anagrams in a String
 // Platform: LeetCode
 // https://leetcode.com/problems/find-all-anagrams-in-a-string/
+
 import java.util.*;
-class FindAllAnagrams { public List<Integer> findAnagrams(String s,String p){List<Integer>r=new ArrayList<>();if(s.length()<p.length())return r;int[]a=new int[26],b=new int[26];for(char c:p.toCharArray())a[c-'a']++;for(int i=0;i<s.length();i++){b[s.charAt(i)-'a']++;if(i>=p.length())b[s.charAt(i-p.length())-'a']--;if(Arrays.equals(a,b))r.add(i-p.length()+1);}return r;} }
+
+class FindAllAnagrams {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+
+        if (s.length() < p.length()) {
+            return result;
+        }
+
+        int[] need = new int[26];
+        int[] window = new int[26];
+
+        for (char c : p.toCharArray()) {
+            need[c - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            window[s.charAt(i) - 'a']++;
+
+            if (i >= p.length()) {
+                window[s.charAt(i - p.length()) - 'a']--;
+            }
+
+            if (Arrays.equals(need, window)) {
+                result.add(i - p.length() + 1);
+            }
+        }
+
+        return result;
+    }
+}

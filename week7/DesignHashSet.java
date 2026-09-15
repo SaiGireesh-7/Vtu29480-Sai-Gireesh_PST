8b8d5cf02eeffe8586aac2cecf4be7936694cba0
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 09:37:32 +0530

    Add Week 7 Task 5 - Design HashSet

---
+++ week7/DesignHashSet.java
@@ -0,0 +1,10 @@
+// Week 7 - Task 5
+// Question: Design a HashSet without using built-in hash table libraries.
+// Platform: LeetCode
+// https://leetcode.com/problems/design-hashset/
+class MyHashSet {
+    boolean[] set = new boolean[1000001];
+    public void add(int key) { set[key] = true; }
+    public void remove(int key) { set[key] = false; }
+    public boolean contains(int key) { return set[key]; }
+}

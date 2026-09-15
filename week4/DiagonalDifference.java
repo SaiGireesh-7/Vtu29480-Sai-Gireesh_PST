4574d51a1b41cbdfaa30572f1ded1ca3f0c04e49
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:29:02 +0530

    Format Week 4 DiagonalDifference

--- week4/DiagonalDifference.java
+++ week4/DiagonalDifference.java
@@ -2,5 +2,29 @@
 // Question: Diagonal Difference
 // Platform: HackerRank
 // https://www.hackerrank.com/challenges/diagonal-difference/
+
 import java.util.*;
-public class DiagonalDifference { public static void main(String[] args){Scanner sc=new Scanner(System.in);int n=sc.nextInt(),a=0,b=0;for(int i=0;i<n;i++)for(int j=0;j<n;j++){int x=sc.nextInt();if(i==j)a+=x;if(i+j==n-1)b+=x;}System.out.println(Math.abs(a-b));}}
+
+public class DiagonalDifference {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] a = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < n; i++) {
+            x += a[i][i];
+            y += a[i][n - 1 - i];
+        }
+
+        System.out.println(Math.abs(x - y));
+    }
+}

5869c16bfc6a2ff872edeca0f4ffbddb918d744c
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:30:00 +0530

    Format Week 5 AlternatingCharacters

--- week5/AlternatingCharacters.java
+++ week5/AlternatingCharacters.java
@@ -2,5 +2,25 @@
 // Question: Alternating Characters
 // Platform: HackerRank
 // https://www.hackerrank.com/challenges/alternating-characters/
+
 import java.util.*;
-public class AlternatingCharacters { public static void main(String[]a){Scanner s=new Scanner(System.in);int t=s.nextInt();while(t-->0){String x=s.next();int c=0;for(int i=1;i<x.length();i++)if(x.charAt(i)==x.charAt(i-1))c++;System.out.println(c);}}}
+
+public class AlternatingCharacters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String s = sc.next();
+            int count = 0;
+
+            for (int i = 1; i < s.length(); i++) {
+                if (s.charAt(i) == s.charAt(i - 1)) {
+                    count++;
+                }
+            }
+
+            System.out.println(count);
+        }
+    }
+}

f7d0cf40995b8542c28d3e9831a982e338c24801
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 09:39:05 +0530

    Fix Week 7 Task 8 to match HackerRank submission format

--- week7/JavaInheritance2.java
+++ week7/JavaInheritance2.java
@@ -2,12 +2,8 @@
 // Question: Create Arithmetic with add() and Adder that inherits Arithmetic.
 // Platform: HackerRank
 // https://www.hackerrank.com/challenges/java-inheritance-2/
-class Arithmetic { int add(int a, int b) { return a + b; } }
-class Adder extends Arithmetic { }
-public class JavaInheritance2 {
-    public static void main(String[] args) {
-        Adder a = new Adder();
-        System.out.println("My superclass is: Arithmetic");
-        System.out.println(a.add(42, 13) + " " + a.add(10, 10) + " " + a.add(15, 5));
-    }
+class Arithmetic {
+    int add(int a, int b) { return a + b; }
+}
+class Adder extends Arithmetic {
 }

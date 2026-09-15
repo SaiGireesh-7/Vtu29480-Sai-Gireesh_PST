27cd8ca03bc5531f6c6a6f8580c9fde5ff0a6602
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:30:28 +0530

    Format Week 6 RepeatedSubstringPattern

--- week6/RepeatedSubstringPattern.java
+++ week6/RepeatedSubstringPattern.java
@@ -2,4 +2,10 @@
 // Question: Repeated Substring Pattern
 // Platform: LeetCode
 // https://leetcode.com/problems/repeated-substring-pattern/
-class RepeatedSubstringPattern { public boolean repeatedSubstringPattern(String s){String x=s+s;return x.substring(1,x.length()-1).contains(s);} }
+
+class RepeatedSubstringPattern {
+    public boolean repeatedSubstringPattern(String s) {
+        String x = s + s;
+        return x.substring(1, x.length() - 1).contains(s);
+    }
+}

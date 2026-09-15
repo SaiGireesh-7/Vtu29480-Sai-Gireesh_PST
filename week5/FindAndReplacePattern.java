566af713d512853874d8bcb2582c890366dd038a
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:30:10 +0530

    Format Week 5 FindAndReplacePattern

--- week5/FindAndReplacePattern.java
+++ week5/FindAndReplacePattern.java
@@ -2,5 +2,39 @@
 // Question: Find and Replace Pattern
 // Platform: LeetCode
 // https://leetcode.com/problems/find-and-replace-pattern/
+
 import java.util.*;
-class FindAndReplacePattern { public List<String> findAndReplacePattern(String[] words,String p){List<String>r=new ArrayList<>();for(String w:words)if(ok(w,p))r.add(w);return r;}boolean ok(String w,String p){Map<Character,Character>a=new HashMap<>(),b=new HashMap<>();for(int i=0;i<w.length();i++){char x=w.charAt(i),y=p.charAt(i);if((a.containsKey(x)&&a.get(x)!=y)||(b.containsKey(y)&&b.get(y)!=x))return false;a.put(x,y);b.put(y,x);}return true;} }
+
+class FindAndReplacePattern {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> ans = new ArrayList<>();
+
+        for (String word : words) {
+            if (match(word, pattern)) {
+                ans.add(word);
+            }
+        }
+
+        return ans;
+    }
+
+    boolean match(String word, String pattern) {
+        Map<Character, Character> map = new HashMap<>();
+        Set<Character> used = new HashSet<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            char a = word.charAt(i);
+            char b = pattern.charAt(i);
+
+            if (map.containsKey(a)) {
+                if (map.get(a) != b) return false;
+            } else {
+                if (used.contains(b)) return false;
+                map.put(a, b);
+                used.add(b);
+            }
+        }
+
+        return true;
+    }
+}

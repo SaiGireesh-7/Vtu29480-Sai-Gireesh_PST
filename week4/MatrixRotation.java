a9d8b12c8a2f764fff9ec919b3de4c0c7d29cdf5
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:29:27 +0530

    Format Week 4 MatrixRotation

--- week4/MatrixRotation.java
+++ week4/MatrixRotation.java
@@ -1,6 +1,51 @@
 // Week 4 Task 10
-// Question: Matrix Layer Rotation
+// Question: Matrix Rotation
 // Platform: HackerRank
 // https://www.hackerrank.com/challenges/matrix-rotation-algo/
+
 import java.util.*;
-public class MatrixRotation { public static void main(String[] args){Scanner sc=new Scanner(System.in);int m=sc.nextInt(),n=sc.nextInt(),r=sc.nextInt();int[][]a=new int[m][n];for(int[]x:a)for(int j=0;j<n;j++)x[j]=sc.nextInt();for(int layer=0;layer<Math.min(m,n)/2;layer++){ArrayList<Integer>v=new ArrayList<>();for(int i=layer;i<m-layer;i++)v.add(a[i][layer]);for(int j=layer+1;j<n-layer;j++)v.add(a[m-layer-1][j]);for(int i=m-layer-2;i>=layer;i--)v.add(a[i][n-layer-1]);for(int j=n-layer-2;j>layer;j--)v.add(a[layer][j]);int q=r%v.size(),z=0;for(int i=layer;i<m-layer;i++)a[i][layer]=v.get((z++ + q)%v.size());for(int j=layer+1;j<n-layer;j++)a[m-layer-1][j]=v.get((z++ + q)%v.size());for(int i=m-layer-2;i>=layer;i--)a[i][n-layer-1]=v.get((z++ + q)%v.size());for(int j=n-layer-2;j>layer;j--)a[layer][j]=v.get((z++ + q)%v.size());}for(int[]x:a){for(int j=0;j<n;j++)System.out.print(x[j]+" ");System.out.println();}}}
+
+public class MatrixRotation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        int[][] a = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int layer = 0; layer < Math.min(m, n) / 2; layer++) {
+            int top = layer;
+            int left = layer;
+            int bottom = m - 1 - layer;
+            int right = n - 1 - layer;
+            int len = 2 * (bottom - top + right - left);
+            int shift = r % len;
+            int[] temp = new int[len];
+            int p = 0;
+
+            for (int i = top; i <= bottom; i++) temp[p++] = a[i][left];
+            for (int j = left + 1; j <= right; j++) temp[p++] = a[bottom][j];
+            for (int i = bottom - 1; i >= top; i--) temp[p++] = a[i][right];
+            for (int j = right - 1; j > left; j--) temp[p++] = a[top][j];
+
+            p = 0;
+            for (int i = top; i <= bottom; i++) a[i][left] = temp[(p++ + shift) % len];
+            for (int j = left + 1; j <= right; j++) a[bottom][j] = temp[(p++ + shift) % len];
+            for (int i = bottom - 1; i >= top; i--) a[i][right] = temp[(p++ + shift) % len];
+            for (int j = right - 1; j > left; j--) a[top][j] = temp[(p++ + shift) % len];
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}

89219b93cc9ccb70448fa77f3652d058ce59b2d7
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:29:56 +0530

    Format Week 5 StringToIntegerAtoi

--- week5/StringToIntegerAtoi.java
+++ week5/StringToIntegerAtoi.java
@@ -2,4 +2,33 @@
 // Question: String to Integer (atoi)
 // Platform: LeetCode
 // https://leetcode.com/problems/string-to-integer-atoi/
-class StringToIntegerAtoi { public int myAtoi(String s){int i=0,n=s.length(),sign=1,res=0;while(i<n&&s.charAt(i)==' ')i++;if(i<n&&(s.charAt(i)=='+'||s.charAt(i)=='-')){if(s.charAt(i)=='-')sign=-1;i++;}while(i<n&&Character.isDigit(s.charAt(i))){int d=s.charAt(i++)-'0';if(res>(Integer.MAX_VALUE-d)/10)return sign==1?Integer.MAX_VALUE:Integer.MIN_VALUE;res=res*10+d;}return res*sign;} }
+
+class StringToIntegerAtoi {
+    public int myAtoi(String s) {
+        int i = 0;
+        int sign = 1;
+        int ans = 0;
+
+        while (i < s.length() && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        if (i < s.length() && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            if (s.charAt(i) == '-') sign = -1;
+            i++;
+        }
+
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            int d = s.charAt(i) - '0';
+
+            if (ans > (Integer.MAX_VALUE - d) / 10) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            ans = ans * 10 + d;
+            i++;
+        }
+
+        return ans * sign;
+    }
+}

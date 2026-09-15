fdcf3c70c8c5581fd41b66ff025e496b2255be58
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:28:27 +0530

    Format Week 4 CompareTheTriplets

--- week4/CompareTheTriplets.java
+++ week4/CompareTheTriplets.java
@@ -2,5 +2,26 @@
 // Question: Compare the Triplets
 // Platform: HackerRank
 // https://www.hackerrank.com/challenges/compare-the-triplets/problem
+
 import java.util.*;
-public class CompareTheTriplets { public static void main(String[] args){Scanner sc=new Scanner(System.in);int a=0,b=0;for(int i=0;i<3;i++){int x=sc.nextInt(),y=sc.nextInt();if(x>y)a++;else if(x<y)b++;}System.out.println(a+" "+b);}}
+
+public class CompareTheTriplets {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 0;
+        int b = 0;
+
+        for (int i = 0; i < 3; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            if (x > y) {
+                a++;
+            } else if (x < y) {
+                b++;
+            }
+        }
+
+        System.out.println(a + " " + b);
+    }
+}

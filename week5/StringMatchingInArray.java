b41233a92c4ef90bfb1ba1f27e8b5f656404a0f5
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:30:14 +0530

    Format Week 5 StringMatchingInArray

--- week5/StringMatchingInArray.java
+++ week5/StringMatchingInArray.java
@@ -2,5 +2,22 @@
 // Question: String Matching in an Array
 // Platform: LeetCode
 // https://leetcode.com/problems/string-matching-in-an-array/
+
 import java.util.*;
-class StringMatchingInArray { public List<String> stringMatching(String[]w){List<String>r=new ArrayList<>();for(int i=0;i<w.length;i++){for(int j=0;j<w.length;j++)if(i!=j&&w[j].contains(w[i])){r.add(w[i]);break;}}return r;} }
+
+class StringMatchingInArray {
+    public List<String> stringMatching(String[] words) {
+        List<String> ans = new ArrayList<>();
+
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words.length; j++) {
+                if (i != j && words[j].contains(words[i])) {
+                    ans.add(words[i]);
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}

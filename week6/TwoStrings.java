d90596e7ac7e620e0b2024d65aa0279fb67aef14
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:30:32 +0530

    Format Week 6 TwoStrings

--- week6/TwoStrings.java
+++ week6/TwoStrings.java
@@ -2,5 +2,27 @@
 // Question: Two Strings
 // Platform: HackerRank
 // https://www.hackerrank.com/challenges/two-strings/
+
 import java.util.*;
-public class TwoStrings { public static void main(String[]a){Scanner s=new Scanner(System.in);int t=s.nextInt();while(t-->0){String x=s.next(),y=s.next();boolean ok=false;for(char c='a';c<='z';c++)if(x.indexOf(c)>=0&&y.indexOf(c)>=0)ok=true;System.out.println(ok?"YES":"NO");}}}
+
+public class TwoStrings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String a = sc.next();
+            String b = sc.next();
+            boolean found = false;
+
+            for (char c = 'a'; c <= 'z'; c++) {
+                if (a.indexOf(c) >= 0 && b.indexOf(c) >= 0) {
+                    found = true;
+                    break;
+                }
+            }
+
+            System.out.println(found ? "YES" : "NO");
+        }
+    }
+}

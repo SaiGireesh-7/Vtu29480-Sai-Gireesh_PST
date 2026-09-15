51486407547639622d16cd73f7a8629c925e76da
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:30:05 +0530

    Format Week 5 LongestSubstringWithoutRepeating

--- week5/LongestSubstringWithoutRepeating.java
+++ week5/LongestSubstringWithoutRepeating.java
@@ -2,5 +2,25 @@
 // Question: Longest Substring Without Repeating Characters
 // Platform: LeetCode
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
 import java.util.*;
-class LongestSubstringWithoutRepeating { public int lengthOfLongestSubstring(String s){Set<Character>set=new HashSet<>();int l=0,ans=0;for(int r=0;r<s.length();r++){while(set.contains(s.charAt(r)))set.remove(s.charAt(l++));set.add(s.charAt(r));ans=Math.max(ans,r-l+1);}return ans;} }
+
+class LongestSubstringWithoutRepeating {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int left = 0;
+        int answer = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(s.charAt(right));
+            answer = Math.max(answer, right - left + 1);
+        }
+
+        return answer;
+    }
+}

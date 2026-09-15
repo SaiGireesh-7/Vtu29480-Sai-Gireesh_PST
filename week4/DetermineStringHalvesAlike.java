72d8930c366abac82b32b82d3d07c08fd77b394f
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:28:52 +0530

    Format Week 4 DetermineStringHalvesAlike

--- week4/DetermineStringHalvesAlike.java
+++ week4/DetermineStringHalvesAlike.java
@@ -2,4 +2,26 @@
 // Question: Determine if String Halves Are Alike
 // Platform: LeetCode
 // https://leetcode.com/problems/determine-if-string-halves-are-alike/
-class DetermineStringHalvesAlike { public boolean halvesAreAlike(String s) { int n=s.length()/2,a=0,b=0; String v="aeiouAEIOU"; for(int i=0;i<n;i++) if(v.indexOf(s.charAt(i))>=0)a++; for(int i=n;i<s.length();i++) if(v.indexOf(s.charAt(i))>=0)b++; return a==b; } }
+
+class DetermineStringHalvesAlike {
+    public boolean halvesAreAlike(String s) {
+        int n = s.length() / 2;
+        int a = 0;
+        int b = 0;
+        String v = "aeiouAEIOU";
+
+        for (int i = 0; i < n; i++) {
+            if (v.indexOf(s.charAt(i)) >= 0) {
+                a++;
+            }
+        }
+
+        for (int i = n; i < s.length(); i++) {
+            if (v.indexOf(s.charAt(i)) >= 0) {
+                b++;
+            }
+        }
+
+        return a == b;
+    }
+}

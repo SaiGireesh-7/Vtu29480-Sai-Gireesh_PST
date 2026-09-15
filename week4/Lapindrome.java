353ba5e74cf64e05bac74f2a14cf72330a6a0dd8
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:28:58 +0530

    Format Week 4 Lapindrome

--- week4/Lapindrome.java
+++ week4/Lapindrome.java
@@ -1,6 +1,37 @@
 // Week 4 Task 2
-// Question: Lapindrome - check whether the first and second halves have the same character frequencies.
+// Question: Lapindrome
 // Platform: CodeChef
 // https://www.codechef.com/problems/LAPIN
+
 import java.util.*;
-public class Lapindrome { public static void main(String[] args){ Scanner sc=new Scanner(System.in); int t=sc.nextInt(); while(t-->0){String s=sc.next(); int n=s.length(),m=n/2; int[] a=new int[26]; for(int i=0;i<m;i++)a[s.charAt(i)-'a']++; for(int i=(n+1)/2;i<n;i++)a[s.charAt(i)-'a']--; boolean ok=true; for(int x:a)if(x!=0)ok=false; System.out.println(ok?"YES":"NO");}}}
+
+public class Lapindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String s = sc.next();
+            int n = s.length();
+            int m = n / 2;
+            int[] a = new int[26];
+
+            for (int i = 0; i < m; i++) {
+                a[s.charAt(i) - 'a']++;
+            }
+
+            for (int i = (n + 1) / 2; i < n; i++) {
+                a[s.charAt(i) - 'a']--;
+            }
+
+            boolean ok = true;
+            for (int x : a) {
+                if (x != 0) {
+                    ok = false;
+                }
+            }
+
+            System.out.println(ok ? "YES" : "NO");
+        }
+    }
+}

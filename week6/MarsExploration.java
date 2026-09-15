4a275ea43dd17ef8a290ce61451fdbc0feef078b
SaiGireesh-7 <vtu29480@veltech.edu.in>
2026-09-15 10:30:39 +0530

    Format Week 6 MarsExploration

--- week6/MarsExploration.java
+++ week6/MarsExploration.java
@@ -2,5 +2,22 @@
 // Question: Mars Exploration
 // Platform: HackerRank
 // https://www.hackerrank.com/challenges/mars-exploration/
+
 import java.util.*;
-public class MarsExploration { public static void main(String[]a){Scanner s=new Scanner(System.in);String x=s.next();String p="SOS";int c=0;for(int i=0;i<x.length();i++)if(x.charAt(i)!=p.charAt(i%3))c++;System.out.println(c);}}
+
+public class MarsExploration {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String pattern = "SOS";
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != pattern.charAt(i % 3)) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
